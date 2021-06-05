955cbb8288ac7f9f99f309252f888140e6df51aa
Jason Benfield <jbenfield108@gmail.com>
2021-06-05 09:49:43 -0400

    Run processes

---
+++ Tests/XTI_Processes.TestAppConfig/TestAppData.cs
@@ -0,0 +1,9 @@
+﻿namespace XTI_Processes.TestAppConfig
+{
+    public sealed class TestAppData
+    {
+        public string Output { get; set; }
+        public string Environment { get; set; }
+        public NamedOptions NamedOptions { get; set; }
+    }
+}
