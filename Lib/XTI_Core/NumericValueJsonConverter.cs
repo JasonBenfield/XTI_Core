9a0c0b18e5b1ee09922ba9acf95a84b574c44fcd
Jason Benfield <jbenfield108@gmail.com>
2021-09-10 23:05:32 -0400

    numeric value json converter

---
+++ Lib/XTI_Core/NumericValueJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace XTI_Core
+{
+    public sealed class NumericValueJsonConverter : JsonConverter<NumericValue>
+    {
+        public override bool CanConvert(Type typeToConvert)
+            => typeof(NumericValue).IsAssignableFrom(typeToConvert);
+
+        public override NumericValue Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException("Expected StartObject token");
+            }
+            int value = 0;
+            while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
+            {
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException("Expected PropertyName token");
+                }
+                var propName = reader.GetString();
+                reader.Read();
+                if (propName == "Value")
+                {
+                    value = reader.GetInt32();
+                }
+            }
+            var valuesField = typeToConvert.GetField("Values", BindingFlags.Static | BindingFlags.Public);
+            var values = valuesField.GetValue(typeToConvert);
+            var valueMethod = valuesField.FieldType.GetMethod("Value", new[] { typeof(int) });
+            var numericValue = valueMethod.Invoke(values, new[] { (object)value });
+            return (NumericValue)numericValue;
+        }
+
+        public override void Write(Utf8JsonWriter writer, NumericValue value, JsonSerializerOptions options)
+        {
+            var writeOptions = new JsonSerializerOptions(options);
+            writeOptions.Converters.Remove(this);
+            JsonSerializer.Serialize(writer, value, value.GetType(), writeOptions);
+        }
+    }
+}
---
+++ Tests/XTI_Core.Tests/SerializeNumericValueTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Text.Json;
+
+namespace XTI_Core.Tests
+{
+    public sealed class SerializeNumericValueTest
+    {
+        [Test]
+        public void ShouldDeserializeNumericValue()
+        {
+            var employee = new Employee
+            {
+                Name = "Xartogg",
+                EmployeeType = EmployeeType.Values.Permanent,
+                HourlyWages = 12.34M
+            };
+            var jsonOptions = new JsonSerializerOptions();
+            jsonOptions.Converters.Add(new NumericValueJsonConverter());
+            var serialized = JsonSerializer.Serialize(employee, jsonOptions);
+            var deserialized = JsonSerializer.Deserialize<Employee>(serialized, jsonOptions);
+            Assert.That(deserialized.EmployeeType, Is.EqualTo(EmployeeType.Values.Permanent));
+            Assert.That(deserialized.Name, Is.EqualTo("Xartogg"));
+            Assert.That(deserialized.HourlyWages, Is.EqualTo(12.34));
+        }
+
+        private sealed class Employee
+        {
+            public EmployeeType EmployeeType { get; set; }
+            public string Name { get; set; }
+            public decimal HourlyWages { get; set; }
+        }
+    }
+}
