885f736b92e8e8fb2105d39c62e6b65086ded887
Jason Benfield <jbenfield108@gmail.com>
2021-10-03 19:31:47 -0400

    re-add deleted file

---
+++ Lib/XTI_Schedule/YearlyScheduleOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace XTI_Schedule
+{
+    public sealed class YearlyScheduleOptions
+    {
+        public YearDay[] Days { get; set; } = new YearDay[] { };
+        public TimeRangeOptions[] TimeRanges { get; set; } = new TimeRangeOptions[] { };
+
+        public Schedule ToSchedule()
+            => new Schedule
+            (
+                new YearlySchedule(Days),
+                (TimeRanges ?? new TimeRangeOptions[] { }).Select(tr => tr.ToTimeRange()).ToArray()
+            );
+    }
+}
