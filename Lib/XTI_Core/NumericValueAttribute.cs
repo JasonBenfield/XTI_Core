560182230cf21a86f1999a367bc538aba2d4485d
Jason Benfield <jbenfield108@gmail.com>
2022-08-09 09:21:14 -0400

    add NumericValueAttribute

---
+++ Lib/XTI_Core/NumericValueAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace XTI_Core;
+
+public sealed class NumericValueAttribute : Attribute
+{
+    public NumericValueAttribute(Type dataType)
+    {
+        DataType = dataType;
+    }
+
+    public Type DataType { get; }
+
+    public override string ToString() => $"{nameof(NumericValueAttribute)} {DataType.Name}";
+}
